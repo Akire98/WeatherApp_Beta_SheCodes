830fb5c0c82f7716ae0eaada4a7a982f35da4644
Akire98 <erikaventer1998@gmail.com>
2024-06-09 19:30:56 +0200

    API search functionality added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+function pushWeatherRefresh(response) {
+  let currentTemp = document.querySelector("#currentTemp");
+  let temperature = response.data.temperature.current;
+  currentTemp.innerHTML = Math.round(temperature);
+  let cityInputElement = document.querySelector("#city-entered");
+  cityInputElement.innerHTML = response.data.city;
+}
+
+function citySearch(city) {
+  let apiKey = "ff0ff4f302f20fbae82t7cb5bd46o49d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(pushWeatherRefresh);
+}
+
 function handleSearchEnter(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#user-search-input");
-  let cityInputElement = document.querySelector("#city-entered");
-  cityInputElement.innerHTML = searchInputElement.value;
+
+  citySearch(searchInputElement.value);
 }
 
 let searchFormElement = document.querySelector("#form-search");
 searchFormElement.addEventListener("submit", handleSearchEnter);
+
+citySearch("Johannesburg");
